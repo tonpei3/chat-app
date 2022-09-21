7d17e4a8553eceb4c40ffe0fdf5a7fbc8aeca8e4
tonpei3 <tomatomatomato0080@icloud.com>
2022-09-21 16:42:56 +0900

    room_usersテーブルの作成

---
+++ app/models/room_user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  belongs_to :room
+  belongs_to :user
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable,  :validatable
+  validates :name, presence: true
+
+  has_many :room_users
+  has_many :rooms, through: :room_users
+end
--- db/migrate/20220921053235_create_rooms.rb
+++ db/migrate/20220921053235_create_rooms.rb
@@ -1,8 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-      t.string :name, null: false
-      t.timestamps
+      t.references :room, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
     end
   end
 end
